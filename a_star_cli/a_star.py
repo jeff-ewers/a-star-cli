70d8dedfd509d745536316fd71c670b61c25b5da
Jeff Ewers <89662718+reison-admin@users.noreply.github.com>
2024-09-06 22:27:39 -0500

    Add logic pseudocode

--- a_star_cli/a_star.py
+++ a_star_cli/a_star.py
@@ -3,3 +3,22 @@ import heapq
 class Node:
     def __init__(self, position, g=0, h=0, parent=None):
         pass
+
+open = []
+#heapify
+closed = []
+
+#pop from minheap
+#if node is goal:
+#   retrace and return path
+#add to closed
+#
+#for each neighbor
+#   if not in open:
+#       compute g, h, and f scores
+#       set parent to current node
+#       add to open
+#   if in open:
+#       if current path has lower g score:
+#           update score and parent
+
